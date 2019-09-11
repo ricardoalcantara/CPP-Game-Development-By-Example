20021cfc910a5e60657667bf5c3e8f35a9952a35
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2019-09-11 00:01:55 -0300

    Chapter06 compiling

---
+++ Chapter08/8.OpenGLProject/premake5.lua
@@ -0,0 +1,63 @@
+project "8.OpenGLProject"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++11"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    systemversion "latest"
+
+    files
+    {
+        "OpenGLProject/*.h",
+        "OpenGLProject/*.cpp",
+        "%{IncludeDir.glm}/glm/**.hpp",
+		"%{IncludeDir.glm}/glm/**.inl",
+    }
+
+    includedirs
+    {
+        "OpenGLProject"
+    }
+
+    links 
+    { 
+        "GLFW",
+        "GLEW",
+    }
+
+    filter "system:windows"
+        defines
+        {
+            "_WIN32",
+            "_WINDOWS",
+            "_CRT_SECURE_NO_WARNINGS",
+        }
+        links 
+        { 
+            "OpenGL32"
+        }
+    
+    filter "system:linux"
+        defines
+        {
+            "_LINUX",
+        }
+        links 
+        { 
+            "GL",
+            "X11", --https://stackoverflow.com/a/17772217
+            "Xxf86vm",
+            "Xrandr",
+            "pthread",
+            "Xi", --
+            "dl", -- http://discourse.glfw.org/t/how-to-prevent-glfw-from-trying-to-link-vulkan-functions/839
+        }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        optimize "On"
--- premake5.lua
+++ premake5.lua
@@ -10,17 +10,19 @@ workspace "CPPGameDevelopmentByExample"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 IncludeDir = {}
--- IncludeDir["GLFW"] = "%{wks.location}/libs/glfw-3.3/include"
--- IncludeDir["Glad"] = "%{wks.location}/libs/glad/include"
--- IncludeDir["glm"] = "%{wks.location}/libs/glm"
--- IncludeDir["stb_image"] = "Hazel/vendor/stb_image"
+IncludeDir["GLFW"] = "%{wks.location}/libs/glfw-3.3/include"
+IncludeDir["GLEW"] = "%{wks.location}/libs/glew-2.1.0/include"
+IncludeDir["glm"] = "%{wks.location}/libs/glm"
+
 
 group "Dependencies"
-    -- include "libs/glfw-3.3"
-    -- include "libs/glad"
-    -- include "libs/spdlog-1.x"
+    include "libs/glew-2.1.0"
+    include "libs/glfw-3.3"
     include "Chapter03/3.SFMLProject"
     include "Chapter04/4.SFMLProject"
     include "Chapter05/5.SFMLProject"
+    include "Chapter06/6.OpenGLProject"
+    -- include "Chapter07/7.OpenGLProject"
+    -- include "Chapter08/8.OpenGLProject"
     
 
